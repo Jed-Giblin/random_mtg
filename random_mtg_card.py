6b08529b296dac2f34b0dac3a1b0e7dd62b65cd7
jed giblin <jed.giblin@odincc.net>
2020-11-05 16:17:03 -0500

    added email

--- random_mtg_card.py
+++ random_mtg_card.py
@@ -1,10 +1,16 @@
 from tap import Tap
 import requests
 import jinja2
+import shutil
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 
 class RandomMtgArgs(Tap):
     color: str = None
     mode: str = 'text'
+    from_addr: str
+    to_addr: str
     template_dir: str = './'
 
     def configure(self):
@@ -26,20 +32,41 @@ def fetch_card_info(args: dict) -> tuple:
 def fetch_card_image(args):
     res = __get('https://api.scryfall.com/cards/random', params=args, headers={'Content-Type':'application/json'})
     if res.status_code < 400:
-        return True, res.url
+        filename = "./{}".format(res.url.split('/')[-1].split('?')[0])
+        image = requests.get(res.url, stream=True)
+        with open(filename, 'wb') as f:
+            image.raw.decode_content = True
+            shutil.copyfileobj(image.raw, f)
+        return True, filename
     else:
         return False, res.status_code
 
 
-def print_card_image(template_dir: str, success: bool, url: str):
+def print_card_image(to_addr:str, from_addr:str, template_dir: str, success: bool, url: str):
     template_loader = jinja2.FileSystemLoader(searchpath=template_dir)
     template_env = jinja2.Environment(loader=template_loader)
     template = template_env.get_template("card_image.jinja.html")
-    print(template.render(url=url,success=success))
+    body = template.render(url=url,success=success)
+    message = MIMEMultipart()
+    message['Subject'] = "MTG Card of the Day"
+    message['From'] = from_addr
+    message['To'] = to_addr
+    message.attach(MIMEText(body, "html"))
+    fp = open(url, 'rb')
+    msgImage = MIMEImage(fp.read())
+    fp.close()
+    msgImage.add_header('Content-ID', '<image1>')
+    message.attach(msgImage)
+    import smtplib
+    smtp = smtplib.SMTP()
+    smtp.connect('localhost')
+    smtp.sendmail(from_addr,to_addr, message.as_string())
+    smtp.quit()
 
 
 def print_card_details():
     pass
+
 
 def main(args: RandomMtgArgs):
     request_args = {'format': args.mode}
@@ -49,7 +76,7 @@ def main(args: RandomMtgArgs):
     if args.mode == 'image':
         request_args['format'] = args.mode
         success, url = fetch_card_image(request_args)
-        print_card_image(args.template_dir, success, url)
+        print_card_image(args.to_addr, args.from_addr, args.template_dir, success, url)
     else:
         print_card_details(fetch_card_info(request_args))
 
